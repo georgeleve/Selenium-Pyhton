463ff332dd2f91dd3be77cb74537272ffdd4d19d
George Leventopoulos <leventopoulos.george@gmail.com>
2020-12-06 21:29:01 +0200

    Update task1_3b.py

--- task1_3b.py
+++ task1_3b.py
@@ -6,10 +6,9 @@ import time
 driver = webdriver.Chrome("C:\\Program Files (x86)\\chromedriver.exe")
 driver.get("https://www.gmail.com")
 
-driver.find_element_by_name("identifier").send_keys("george.george5000@gmail.com")  # gmail 
+driver.find_element_by_name("identifier").send_keys("yourgmail@gmail.com")  # gmail 
 driver.find_element_by_xpath("//*[@id='identifierNext']/div/span/span").click()     # next
 #problem here , google doesn't let me type my password
 time.sleep(2)
-driver.find_element_by_name("password").send_keys("Ojbeinaidasdd@!")           # password
+driver.find_element_by_name("password").send_keys("yourpassword")           # password
 driver.find_element_by_xpath("//*[@id='passwordNext']/div/span/span").click() # next
-
