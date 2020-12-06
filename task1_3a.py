95175d0e0c5577692f48c22677b20ce8445c27a8
George Leventopoulos <leventopoulos.george@gmail.com>
2020-12-06 21:28:06 +0200

    Update task1_3a.py

--- task1_3a.py
+++ task1_3a.py
@@ -6,8 +6,8 @@ import time
 driver = webdriver.Chrome("C:\\Program Files (x86)\\chromedriver.exe")
 driver.get("https://www.facebook.com")
 
-driver.find_element_by_name("email").send_keys("george.george5000@gmail.com") # email
+driver.find_element_by_name("email").send_keys("yourgmail@gmail.com") # email
 time.sleep(1) # stop for 1sec
-driver.find_element_by_name("pass").send_keys("password#31") # password
+driver.find_element_by_name("pass").send_keys("your password") # password
 time.sleep(1) 
 driver.find_element_by_xpath("//*[@id='u_0_d']").click() # click login
